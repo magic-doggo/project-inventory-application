17cca712dba536ecec2654e202627bf1bf9da527
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-06-28 08:26:22 +0000

    Add navbar and icon

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 require('dotenv').config();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 
 const indexRouter = require("./routes/indexRouter");
 
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,10 +2,14 @@ const db = require("../db/queries");
 
 async function getItems(req, res) {
     const items = await db.getAllItems();
+    // let items2 = [];
+    // for (let i = 0; i < 2; i++) {
+    //     items2.push(items[i])
+    // } //if want to show less items?
     console.log(items);
     res.render("index", {
         title: "index",
-        items: items
+        items: items,
     })
 }
 
