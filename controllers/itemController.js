2c7dac8f0dfd3bff46b7ed45604fdaa1a1218390
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-06-29 07:56:14 +0000

    Add price radio buttons which change the req.query

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,8 +1,10 @@
 const db = require("../db/queries");
 
 async function getItems(req, res) {
+    //maybe if tags/some other filters exist, items = not get all items but some other filter
     const items = await db.getAllItems();
     let selectedTag = req.query.tag;
+    let selectedPriceSort = req.query.priceSort;
     // let items2 = [];
     // for (let i = 0; i < 2; i++) {
     //     items2.push(items[i])
@@ -11,8 +13,13 @@ async function getItems(req, res) {
     res.render("index", {
         title: "index",
         items: items,
-        selectedTag: selectedTag
+        selectedTag: selectedTag,
+        selectedPriceSort: selectedPriceSort
     })
+}
+
+function filterByTag(tag) {
+
 }
 
 module.exports = {
--- db/queries.js
+++ db/queries.js
@@ -1,9 +1,18 @@
 const pool = require("./pool");
 
 async function getAllItems() {
-    const {rows} = await pool.query("SELECT * FROM lol_items LIMIT 10");
+    const {rows} = await pool.query("SELECT * FROM lol_items where normalStoreItemBool = True LIMIT 10");
     return rows;
 }
+
+//get items and their tag
+// SELECT id, name, item_tag
+// FROM lol_items
+// LEFT JOIN item_tags
+// ON id = item_id
+// WHERE normalStoreItemBool = True;
+
+
 
 module.exports = {
     getAllItems
