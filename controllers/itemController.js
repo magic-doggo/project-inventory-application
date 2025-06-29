a478a289f966d9817030b36ca8f0d182ee49f751
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-06-29 07:09:02 +0000

    Add tag form which can query based on item tag

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,6 +2,7 @@ const db = require("../db/queries");
 
 async function getItems(req, res) {
     const items = await db.getAllItems();
+    let selectedTag = req.query.tag;
     // let items2 = [];
     // for (let i = 0; i < 2; i++) {
     //     items2.push(items[i])
@@ -10,6 +11,7 @@ async function getItems(req, res) {
     res.render("index", {
         title: "index",
         items: items,
+        selectedTag: selectedTag
     })
 }
 
